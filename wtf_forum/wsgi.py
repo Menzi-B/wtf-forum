d46faca1edff4624ffffd76a5fa3a70458ad044f
Menzi <111010986+Menzi-B@users.noreply.github.com>
2024-03-31 01:11:11 +0200

    Update wsgi.py

--- wtf_forum/wsgi.py
+++ wtf_forum/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wtf.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wtf_forum.settings')
 
 application = get_wsgi_application()
 
