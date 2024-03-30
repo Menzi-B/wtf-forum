ddd686b32275bee239431e48a0c7dbf04559c288
Menzi <111010986+Menzi-B@users.noreply.github.com>
2024-03-31 00:11:47 +0200

    Update views.py
    Added index view

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,18 @@
+from datetime import datetime
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def article_list(request):
     return render(request, 'articles/articles_list.html')
+
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
