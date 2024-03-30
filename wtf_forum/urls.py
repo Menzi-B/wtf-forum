daf67bebb9cc98106003ef9cb9c07ad32f455d63
Menzi <111010986+Menzi-B@users.noreply.github.com>
2024-03-31 00:36:09 +0200

    Update urls.py

--- wtf_forum/urls.py
+++ wtf_forum/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from articles import views as article_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('articles.urls')),
+    path('articles/', include('articles.urls')),
+    path('', article_views.article_list, name='home'),
 ]
