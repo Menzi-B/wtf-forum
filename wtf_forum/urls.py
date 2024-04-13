90929ea6bf6046a860ab6ebfd82603292de5c67b
Menzi <111010986+Menzi-B@users.noreply.github.com>
2024-04-14 00:18:45 +0200

    Update urls.py

--- wtf_forum/urls.py
+++ wtf_forum/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 from articles import views as article_views
 
 urlpatterns = [
